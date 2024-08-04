54d1e7baa3f21b8aaad40d18c10c0c8be9b84447
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-08-05 00:31:38 +0700

    Add Stopwatch logic

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS426_Final_Project
 This is our mobile application project for the final term of CS426
+
+Project Name: Focus Tree
--- app/src/main/java/com/hfad/cs426_final_project/MainActivity.java
+++ app/src/main/java/com/hfad/cs426_final_project/MainActivity.java
@@ -1,6 +1,8 @@
 package com.hfad.cs426_final_project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View view) {
+        final TextView timeView = findViewById(R.id.time_view);
+        int hours = seconds/3600;
+        int minutes = (seconds%3600)/60;
+        int secs = seconds%60;
+        String time = String.format(Locale.getDefault(),
+                "%d:%02d:%02d", hours, minutes, secs);
+        timeView.setText(time);
+        if (running) {
+            seconds++;
+        }
     }
 }
