dc05940cffefdcd61f5cc099ecd42f2187a49479
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-08-05 08:58:56 +0700

    Add Stopwatch display and buttons to plant and stop

---
+++ app/src/main/java/com/hfad/cs426_final_project/MainScreenActivity.java
@@ -0,0 +1,72 @@
+package com.hfad.cs426_final_project;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class MainScreenActivity extends AppCompatActivity {
+    //Number of seconds displayed on the stopwatch.
+    private int seconds = 0;
+    //Is the stopwatch running?
+    private boolean running;
+
+    TextView timeView;
+    Button startButton;
+    Button stopButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        getUIReferences();
+        runTimer();
+    }
+
+    private void getUIReferences() {
+        timeView = findViewById(R.id.time_view);
+        startButton = findViewById(R.id.start_button);
+        stopButton = findViewById(R.id.stop_button);
+    }
+
+    //Start the stopwatch running when the Start button is clicked.
+    public void onClickStart(View view) {
+        running = true;
+        startButton.setVisibility(View.GONE);
+        stopButton.setVisibility(View.VISIBLE);
+    }
+    //Stop the stopwatch running when the Stop button is clicked.
+    public void onClickStop(View view) {
+        running = false;
+        stopButton.setVisibility(View.GONE);
+        startButton.setVisibility(View.VISIBLE);
+    }
+
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                // 1 second = 1000 milliseconds
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+
+}
