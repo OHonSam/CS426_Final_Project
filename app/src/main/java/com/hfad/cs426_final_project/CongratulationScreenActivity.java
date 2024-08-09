c35618330d78acbf7ec831c820ab87fd915474fd
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-08-09 15:54:30 +0700

    Add UI component

---
+++ app/src/main/java/com/hfad/cs426_final_project/CongratulationScreenActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.cs426_final_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CongratulationScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congratulation_screen);
+    }
+}
