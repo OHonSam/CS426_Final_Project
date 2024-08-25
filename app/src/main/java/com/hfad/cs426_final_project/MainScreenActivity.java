589786e776a33f14224c77f4df7190af54af5939
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-08-25 21:41:29 +0700

    Nav Drawer now works with toggle effect

--- app/src/main/java/com/hfad/cs426_final_project/MainScreenActivity.java
+++ app/src/main/java/com/hfad/cs426_final_project/MainScreenActivity.java
@@ -4,10 +4,13 @@ import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.MotionEvent;
+import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroupOverlay;
@@ -16,13 +19,29 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.hfad.cs426_final_project.CustomUIComponent.ClickableImageView;
 import com.hfad.cs426_final_project.CustomUIComponent.MyButton;
 
-public class MainScreenActivity extends AppCompatActivity {
+import java.util.Locale;
+import java.util.Objects;
+
+public class MainScreenActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //Number of seconds displayed on the stopwatch.
+    private int seconds = 0;
+    //Is the stopwatch running?
+    private boolean running;
+
     TextView timeView;
     MyButton startButton, todoButton, musicButton;
     ClickableImageView todoImage, musicImage;
@@ -31,6 +50,10 @@ public class MainScreenActivity extends AppCompatActivity {
     ConstraintLayout popupMusicContainer;
 
     @SuppressLint("ClickableViewAccessibility")
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,8 +62,12 @@ public class MainScreenActivity extends AppCompatActivity {
 
         setupMusicListener();
         setupTodoListener();
-    }
+    
 
+        setupToolbar();
+        setupNavigationDrawer();
+    }
+    
     private void showMusicSelectionPopup() {
         // Inflate the popup layout
         View popupView = getLayoutInflater().inflate(R.layout.popup_music_selection, null);
@@ -88,6 +115,23 @@ public class MainScreenActivity extends AppCompatActivity {
         overlay.clear();
     }
 
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("");
+    }
+
+    private void setupNavigationDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer,
+                toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
     private void getUIReferences() {
         timeView = findViewById(R.id.time_view);
         startButton = findViewById(R.id.plant_button);
@@ -98,6 +142,9 @@ public class MainScreenActivity extends AppCompatActivity {
         musicImage =findViewById(R.id.music_image);
         musicButton=findViewById(R.id.music_button);
         popupMusicContainer = findViewById(R.id.main);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_view_screen_choices);
+        drawer = findViewById(R.id.drawer_layout_main_screen);
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -126,6 +173,7 @@ public class MainScreenActivity extends AppCompatActivity {
         musicContainer.setOnClickListener(musicClickListener);
         musicButton.setOnClickListener(musicClickListener);
         musicImage.setOnClickListener(musicClickListener);
+
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -149,4 +197,27 @@ public class MainScreenActivity extends AppCompatActivity {
         todoImage.setOnTouchListener(todoTouchListener);
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        if (id == R.id.nav_store_screen)
+            intent = new Intent(this, StoreScreenActivity.class);
+
+        startActivity(intent);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
