3b24da4691a4b4a766f7cc27470a8fe116e7b9bc
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-08-27 11:52:53 +0700

    Add Noti and Vibrate based on phone sound state

--- app/src/main/java/com/hfad/cs426_final_project/MainScreen/MainScreenActivity.java
+++ app/src/main/java/com/hfad/cs426_final_project/MainScreen/MainScreenActivity.java
@@ -49,6 +49,7 @@ public class MainScreenActivity extends AppCompatActivity implements NavigationV
     ClickableImageView todoImage, musicImage;
     LinearLayout todoContainer, musicContainer;
     Clock clock;
+    boolean clockState;
 
     ConstraintLayout popupMusicContainer;
 
