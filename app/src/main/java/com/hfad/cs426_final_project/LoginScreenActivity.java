5d192148e0b8d8a63c8fa4400078e16a73a629a7
zRain204 <126950997+zRain204@users.noreply.github.com>
2024-08-30 00:22:19 +0700

    Add Login authentication and update layout

--- app/src/main/java/com/hfad/cs426_final_project/LoginScreenActivity.java
+++ app/src/main/java/com/hfad/cs426_final_project/LoginScreenActivity.java
@@ -1,15 +1,27 @@
 package com.hfad.cs426_final_project;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.hfad.cs426_final_project.CustomUIComponent.ClickableImageView;
 import com.hfad.cs426_final_project.CustomUIComponent.MyButton;
 
 public class LoginScreenActivity extends AppCompatActivity {
+    private ClickableImageView btnBack;
+
     private EditText edtEmail;
     private EditText edtPassword;
     private MyButton btnLogin;
@@ -24,16 +36,47 @@ public class LoginScreenActivity extends AppCompatActivity {
     }
 
     private void initUI() {
+        btnBack = findViewById(R.id.backButton_loginScreen);
         edtEmail = findViewById(R.id.emailEditText_loginScreen);
         edtPassword = findViewById(R.id.passwordEditText_loginScreen);
         btnLogin = findViewById(R.id.loginButton_loginScreen);
     }
 
     private void initListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreenActivity.this, WelcomeScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String email = edtEmail.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+                if(email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginScreenActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginScreenActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(LoginScreenActivity.this, MainScreenActivity.class);
+                                    startActivity(intent);
+                                    finishAffinity();
+                                } else {
+                                    Toast.makeText(LoginScreenActivity.this, "Login failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
--- app/src/main/java/com/hfad/cs426_final_project/SignUpScreenActivity.java
+++ app/src/main/java/com/hfad/cs426_final_project/SignUpScreenActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,15 +19,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.hfad.cs426_final_project.CustomUIComponent.ClickableImageView;
 import com.hfad.cs426_final_project.CustomUIComponent.MyButton;
 
 public class SignUpScreenActivity extends AppCompatActivity {
+    private ClickableImageView btnBack;
+
     private EditText edtFullName;
     private EditText edtEmail;
     private EditText edtPassword;
     private EditText edtConfirmPassword;
     private MyButton btnSignUp;
-
+    private TextView tvSignIn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,19 +42,35 @@ public class SignUpScreenActivity extends AppCompatActivity {
     }
 
     private void initUI() {
+        btnBack = findViewById(R.id.backButton_signUpScreen);
         edtFullName = findViewById(R.id.fullNameEditText_signUpScreen);
         edtEmail = findViewById(R.id.emailEditText_signUpScreen);
         edtPassword = findViewById(R.id.passwordEditText_signUpScreen);
         edtConfirmPassword = findViewById(R.id.confirmEditText_signUpScreen);
         btnSignUp = findViewById(R.id.signUpButton_signUpScreen);
+        tvSignIn = findViewById(R.id.signInTextView_signUpScreen);
     }
 
     private void initListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpScreenActivity.this, WelcomeScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = edtEmail.getText().toString().trim();
                 String password = edtPassword.getText().toString().trim();
+                if(email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(SignUpScreenActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 FirebaseAuth mAuth = FirebaseAuth.getInstance();
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(SignUpScreenActivity.this, new OnCompleteListener<AuthResult>() {
@@ -66,6 +86,15 @@ public class SignUpScreenActivity extends AppCompatActivity {
                                 }
                             }
                         });
+            }
+        });
+
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpScreenActivity.this, LoginScreenActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
