7d6fbb13001298b87cd6843335252a6854a57730
zRain204 <126950997+zRain204@users.noreply.github.com>
2024-08-30 15:44:37 +0700

    Add user to firebase

--- app/src/main/java/com/hfad/cs426_final_project/SignUpScreenActivity.java
+++ app/src/main/java/com/hfad/cs426_final_project/SignUpScreenActivity.java
@@ -20,6 +20,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.FirebaseUser;
 import com.hfad.cs426_final_project.CustomUIComponent.ClickableImageView;
 import com.hfad.cs426_final_project.CustomUIComponent.MyButton;
 import com.hfad.cs426_final_project.SmartEditText.EmailEditText;
