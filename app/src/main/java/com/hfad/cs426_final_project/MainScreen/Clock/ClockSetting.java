b174d31bab744f4473c07a9ca718ab3bae50ef7a
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-09-03 13:32:38 +0700

    Save state of deep mode, count exceed of timer and stopwatch

--- app/src/main/java/com/hfad/cs426_final_project/MainScreen/Clock/ClockSetting.java
+++ app/src/main/java/com/hfad/cs426_final_project/MainScreen/Clock/ClockSetting.java
@@ -2,39 +2,51 @@ package com.hfad.cs426_final_project.MainScreen.Clock;
 
 public class ClockSetting {
     private Clock.ClockMode clockMode;
-    private boolean isDeepMode;
+    private boolean isDeepModeTimer;
+    private boolean isDeepModeStopwatch;
     private boolean isCountExceedTime;
 
     public ClockSetting(){
         this.clockMode = Clock.ClockMode.TIMER;
-        this.isDeepMode = false;
+        this.isDeepModeTimer = false;
+        this.isDeepModeStopwatch = false;
         this.isCountExceedTime = false;
     }
 
-    public ClockSetting(Clock.ClockMode clockMode, boolean isDeepMode, boolean isCountExceedTime) {
+    public ClockSetting(Clock.ClockMode clockMode, boolean isDeepModeTimer, boolean isDeepModeStopwatch, boolean isCountExceedTime) {
         this.clockMode = clockMode;
-        this.isDeepMode = isDeepMode;
+        this.isDeepModeTimer = isDeepModeTimer;
+        this.isDeepModeStopwatch = isDeepModeStopwatch;
         this.isCountExceedTime = isCountExceedTime;
     }
 
     public Clock.ClockMode getType() {
         return clockMode;
     }
+
     public void setType(Clock.ClockMode type) {
         this.clockMode = type;
     }
+    public boolean getIsDeepModeTimer() {
+        return isDeepModeTimer;
+    }
 
-    public boolean getIsDeepMode() {
-        return isDeepMode;
+    public void setIsDeepModeTimer(boolean deepMode) {
+        isDeepModeTimer = deepMode;
     }
-    public void setIsDeepMode(boolean deepMode) {
-        isDeepMode = deepMode;
-    }
-    
     public boolean getIsCountExceedTime() {
         return isCountExceedTime;
     }
+
     public void setIsCountExceedTime(boolean countExceedTime) {
         isCountExceedTime = countExceedTime;
+    }
+
+    public boolean getIsDeepModeStopwatch() {
+        return isDeepModeStopwatch;
+    }
+
+    public void setIsDeepModeStopwatch(boolean deepModeStopwatch) {
+        isDeepModeStopwatch = deepModeStopwatch;
     }
 }
--- app/src/main/java/com/hfad/cs426_final_project/MainScreen/Clock/StopwatchOptionFragment.java
+++ app/src/main/java/com/hfad/cs426_final_project/MainScreen/Clock/StopwatchOptionFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,16 +14,34 @@ import android.view.ViewGroup;
 import com.hfad.cs426_final_project.R;
 
 public class StopwatchOptionFragment extends Fragment {
+    private SwitchCompat deepFocusSwitch;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stopwatch_option, container, false);
+        View view = inflater.inflate(R.layout.fragment_stopwatch_option, container, false);
+
+        // Retrieve arguments
+        Bundle args = getArguments();
+        if (args != null) {
+            boolean isCurDeepMode = args.getBoolean("isCurDeepModeStopwatch", false);
+
+            // Initialize SwitchCompat components with the correct states
+            deepFocusSwitch = view.findViewById(R.id.deepFocusSwitchButton_stopwatch);
+
+            deepFocusSwitch.setChecked(isCurDeepMode);
+        }
+
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    public boolean isDeepFocusEnabled() {
+        return deepFocusSwitch != null && deepFocusSwitch.isChecked();
     }
 }
