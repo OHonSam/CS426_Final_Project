dbd2bdc5d1a790baa24b1faddc6c207e3569c07f
Rito <116003382+vinhnamhai321@users.noreply.github.com>
2024-09-10 12:08:43 +0700

    FailScreenActivity

--- app/src/main/java/com/hfad/cs426_final_project/MainScreen/Clock/Clock.java
+++ app/src/main/java/com/hfad/cs426_final_project/MainScreen/Clock/Clock.java
@@ -127,7 +127,8 @@ public class Clock {
         int initialTargetMinutes = clockSetting.getTargetTime() / 60;
         int initialProgressIntervalIndex = initialTargetMinutes / PROGRESS_INTERVAL_LEN;
 
-        // Ensure that the initial progress is at least the minimum interval
+        // Ensure that the initial prog
+        // ress is at least the minimum interval
         if (initialProgressIntervalIndex < (PROGRESS_MINUTES_MIN / PROGRESS_INTERVAL_LEN)) {
             initialProgressIntervalIndex = PROGRESS_MINUTES_MIN / PROGRESS_INTERVAL_LEN;
         }
