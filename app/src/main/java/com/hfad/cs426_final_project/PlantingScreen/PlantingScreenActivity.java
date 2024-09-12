52da7984e41e6aa073037b7c40688ff399100f1d
zRain204 <126950997+zRain204@users.noreply.github.com>
2024-09-12 23:47:34 +0700

    Add PlantingBlockAdapter

--- app/src/main/java/com/hfad/cs426_final_project/PlantingScreen/PlantingScreenActivity.java
+++ app/src/main/java/com/hfad/cs426_final_project/PlantingScreen/PlantingScreenActivity.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hfad.cs426_final_project.AppContext;
 import com.hfad.cs426_final_project.BaseScreenActivity;
 import com.hfad.cs426_final_project.CustomUIComponent.ClickableImageView;
+import com.hfad.cs426_final_project.DataStorage.BlockData;
+import com.hfad.cs426_final_project.MainScreen.BottomSheet.OwnBlockAdapter;
 import com.hfad.cs426_final_project.R;
+
+import java.util.List;
 
 public class PlantingScreenActivity extends BaseScreenActivity {
     private HexagonalLandView hexagonalLandView;
@@ -14,15 +22,22 @@ public class PlantingScreenActivity extends BaseScreenActivity {
     private ImageView gardenModeBtn;
     private boolean isPlantingMode = false;
 
+    private RecyclerView rcvLandSelection;
+    private List<BlockData> blockDataList;
+    private BlockData curBlockData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        blockDataList = AppContext.getInstance().getCurrentUser().getOwnBlock();
         initializeComponents();
+        initRCVLandSelection();
     }
 
     private void initializeComponents() {
         gardenModeBtn = findViewById(R.id.garden_mode);
         hexagonalLandView = findViewById(R.id.hexagonalLandView);
+        rcvLandSelection = findViewById(R.id.land_selection);
 
         zoomInButton = findViewById(R.id.zoom_in_btn);
         zoomOutButton = findViewById(R.id.zoom_out_btn);
@@ -33,6 +48,19 @@ public class PlantingScreenActivity extends BaseScreenActivity {
         zoomInButton.setOnClickListener(v -> hexagonalLandView.zoomIn());
         zoomOutButton.setOnClickListener(v -> hexagonalLandView.zoomOut());
         zoomResetButton.setOnClickListener(v -> hexagonalLandView.resetZoom());
+    }
+
+    private void initRCVLandSelection() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rcvLandSelection.setLayoutManager(layoutManager);
+
+        PlantingBlockAdapter plantingBlockAdapter = new PlantingBlockAdapter(blockDataList, new PlantingBlockAdapter.IClickPlantingBlockListener() {
+            @Override
+            public void onClickPlantingBlock(BlockData blockData) {
+                curBlockData = blockData;
+            }
+        });
+        rcvLandSelection.setAdapter(plantingBlockAdapter);
     }
 
     private void toggleGardenMode() {
